630896aad175cc3d1d228346a19ccffe57cbfa28
Katherine <ktchou@ucla.edu>
2024-04-20 15:25:31 -0700

    Center title, no Latin words hehe

--- lahacks24/src/components/past_gratitudes_page/grat.jsx
+++ lahacks24/src/components/past_gratitudes_page/grat.jsx
@@ -5,34 +5,12 @@ import Button from 'react-bootstrap/Button';
 
 import { Outlet } from 'react-router-dom';
 
-
-
 const Gratitudes = () => {
   return (
     <div>
-      <div>
           <div className='titleGrat'>
-            <p className='titleText'>Past Gratitudes.</p>
-          </div>
-        <div class="columns-container">
-          <div class="column">
-            <p>MM/DD/YYYY</p>
-          </div>
-          <div class="column">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-          </div>
-        </div>
-
-        <div class="columns-container">
-          <div class="column">
-            <p>MM/DD/YYYY</p>
-          </div>
-          <div class="column">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-          </div>
-        </div>
-
-      </div>
+            <p className='titleText'>Past Gratitudes.</p> 
+          </div>   
       <Outlet />
     </div>
   )
