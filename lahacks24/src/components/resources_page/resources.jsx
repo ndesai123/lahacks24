4d7295161a018e33da2d99a184e2318efbcb3644
Katherine <ktchou@ucla.edu>
2024-04-20 16:17:13 -0700

    Updates to the text on the resources page

--- lahacks24/src/components/resources_page/resources.jsx
+++ lahacks24/src/components/resources_page/resources.jsx
@@ -5,13 +5,21 @@ import Button from 'react-bootstrap/Button';
 
 import { Outlet } from 'react-router-dom';
 
-
-
 const Resources = () => {
   return (
     <div>
       <div>
-        RESOURCES
+          <div className='titleResources'>
+            <p className='titleTextRe'>Resources.</p>
+          </div>
+          <div class="resourcesList">
+            <p>National Suicide Prevention Lifeline: 1-800-273-TALK (1-800-273-8255)</p>
+            <p>Crisis Text Line: Text HOME to 741741 (in the United States)</p>
+            <p>National Institute of Mental Health (NIMH): 1-866-615-6464</p>
+            <p>National Alliance on Mental Illness (NAMI): 1-800-950-6264</p>
+            <p>Mental Health America: 1-800-273-TALK (1-800-273-8255)</p>
+            <p>Remember, these resources are here to support you, whether you're seeking immediate assistance during a crisis or looking for ongoing mental health care.</p>
+          </div>
       </div>
       <Outlet />
     </div>
