b030be0ebb8d9e7d8cc4d5054dcfae45a16ad6b5
Katherine <ktchou@ucla.edu>
2024-04-20 15:35:26 -0700

    Actual fix for Past Grat Pg

--- lahacks24/src/components/past_gratitudes_page/grat.jsx
+++ lahacks24/src/components/past_gratitudes_page/grat.jsx
@@ -5,12 +5,34 @@ import Button from 'react-bootstrap/Button';
 
 import { Outlet } from 'react-router-dom';
 
+
+
 const Gratitudes = () => {
   return (
     <div>
+      <div>
           <div className='titleGrat'>
-            <p className='titleText'>Past Gratitudes.</p> 
-          </div>   
+            <p className='titleText'>Past Gratitudes.</p>
+          </div>
+        <div class="columns-container">
+          <div class="column">
+            <p>MM/DD/YYYY</p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+        <div class="columns-container">
+          <div class="column">
+            <p>MM/DD/YYYY</p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+
+      </div>
       <Outlet />
     </div>
   )
