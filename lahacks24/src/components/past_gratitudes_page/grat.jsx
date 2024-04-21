8e44cf0a684ad9d173b58f3bda76bed97dd03276
Nemi Desai <nemi@mysamadmin.com>
2024-04-20 18:25:38 -0700

    database pulled

--- lahacks24/src/components/past_gratitudes_page/grat.jsx
+++ lahacks24/src/components/past_gratitudes_page/grat.jsx
@@ -1,41 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./grat.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import { collection, getDocs } from 'firebase/firestore'; // Import Firestore functions for querying data
+import { db } from '../../firebase.js'; // Assuming you have a Firebase configuration file
 
 import { Outlet } from 'react-router-dom';
 
-
-
 const Gratitudes = () => {
+  const [gratitudes, setGratitudes] = useState([]);
+
+  useEffect(() => {
+    const fetchGratitudes = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'entry')); // Replace 'gratitudes' with the name of your Firestore collection
+        const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setGratitudes(data);
+      } catch (error) {
+        console.error('Error fetching gratitudes:', error);
+      }
+    };
+
+    fetchGratitudes();
+  }, []); // Empty dependency array ensures useEffect runs only once on component mount
+
   return (
     <div>
       <div>
-          <div className='titleGrat'>
-            <p className='titleText'>Past Gratitudes.</p>
-          </div>
-        <div class="columns-container">
-          <div class="column">
-            <p>MM/DD/YYYY</p>
-          </div>
-          <div class="column">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-          </div>
+        <div className='titleGrat'>
+          <p className='titleText'>Past Gratitudes</p>
         </div>
-
-        <div class="columns-container">
-          <div class="column">
-            <p>MM/DD/YYYY</p>
+        {gratitudes.map(gratitude => (
+          <div className="columns-container">
+            <div key={gratitude.id} className="column">
+              <p>{gratitude.date}</p>
+            </div>
+            <div key={gratitude.id} className="column">
+              <p>{gratitude.gratitude}</p>
+            </div>
           </div>
-          <div class="column">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-          </div>
-        </div>
-
+        ))}
       </div>
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default Gratitudes
+export default Gratitudes;
