2a79ca65ba72ac27557698f7c9e8534befbe6af1
lachan0705 <lavinachan0705@gmail.com>
2024-04-20 17:00:24 -0700

    sad ducky

--- lahacks24/src/components/pet_page/pet.jsx
+++ lahacks24/src/components/pet_page/pet.jsx
@@ -15,13 +15,13 @@ const Pets = () => {
 
   return (
     <div>
-      <div className="centered"> {/* Changed class to className */}
-        <p>Daily goal: X/3 </p>
-        <p>Lifetime: XX </p>
-        <img className="img-size" src="adult_duck.PNG" alt="Duck"></img> {/* Added alt attribute */}
-        <div>
-          <button>SUBMIT GRATITUDE</button>
-          <button onClick={handleGratClick}>PAST GRATITUDES</button>
+      <div className="centered text-position"> {/* Changed class to className */}
+        <p className="font-size position-daily">Daily goal: X/3 </p>
+        <p className="font-size position-lifetime">Lifetime: XX </p>
+        <img className="img-size position-duck" src="adult_duck.PNG" alt="Duck"></img> {/* Added alt attribute */}
+        <div className="button-container font-size-button">
+          <button className="position-image position-leftbutton">SUBMIT GRATITUDE</button>
+          <button className="position-image position-rightbutton" onClick={handleGratClick}>PAST GRATITUDES</button>
         </div>
       </div>
       <Outlet />
